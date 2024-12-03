abd0b9cd448c6a422ac0fc984b97166344d00788
timothfee <tdsfee@gmail.com>
2024-12-02 22:48:02 -0500

    Init Commit

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -9,20 +9,16 @@ public class DataSaver {
         String choice;
 
         do {
-            // Collect data for one record
             String firstName = SafeInput.getNonEmptyString(in, "Enter First Name");
             String lastName = SafeInput.getNonEmptyString(in, "Enter Last Name");
             String id = String.format("%06d", SafeInput.getRangedInt(in, "Enter ID Number (6 digits)", 0, 999999));
             String email = SafeInput.getNonEmptyString(in, "Enter Email");
             int yearOfBirth = SafeInput.getRangedInt(in, "Enter Year of Birth", 1900, 2024);
 
-            // Add record to the list
             records.add(String.join(", ", firstName, lastName, id, email, String.valueOf(yearOfBirth)));
 
-            // Ask if the user wants to add another record
         } while (SafeInput.getYNConfirm(in, "Add another record?"));
 
-        // Save all records to a file
         String fileName = SafeInput.getNonEmptyString(in, "Enter filename to save (with .csv extension)");
 
         try (PrintWriter writer = new PrintWriter(new File("./src/" + fileName))) {
